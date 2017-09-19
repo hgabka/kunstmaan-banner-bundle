d35c77444c4923813e2b6e4cba3eb0b325d25084
Gabe <hgabka@dev.hgabka.eu>
2017-09-19 14:14:28 +0200

    js javitas

--- src/Resources/public/banner_edit.js
+++ src/Resources/public/banner_edit.js
@@ -1,22 +1,24 @@
 $(function() {
    $('input[name="hgabka_kunstmaanbanner_banner_type[type]"]').change(function() {
       var type = $('input[name="hgabka_kunstmaanbanner_banner_type[type]"]:checked').val();
+
+      hideElem('#hgabka_kunstmaanbanner_banner_type_media');
+      hideElem('#hgabka_kunstmaanbanner_banner_type_hoverMedia');
+      hideElem('#hgabka_kunstmaanbanner_banner_type_imageAlt');
+      hideElem('#hgabka_kunstmaanbanner_banner_type_imageTitle');
+      hideElem('#hgabka_kunstmaanbanner_banner_type_url_link_url');
+      hideElem('#hgabka_kunstmaanbanner_banner_type_newWindow');
+      hideElem('#hgabka_kunstmaanbanner_banner_type_html');
+
       if (type=='html') {
-          hideElem('#hgabka_kunstmaanbanner_banner_type_media');
-          hideElem('#hgabka_kunstmaanbanner_banner_type_hoverMedia');
-          hideElem('#hgabka_kunstmaanbanner_banner_type_imageAlt');
-          hideElem('#hgabka_kunstmaanbanner_banner_type_imageTitle');
-          hideElem('#hgabka_kunstmaanbanner_banner_type_url_link_url');
-          hideElem('#hgabka_kunstmaanbanner_banner_type_newWindow');
           showElem('#hgabka_kunstmaanbanner_banner_type_html');
-      } else {
+      } else if (type=='image') {
           showElem('#hgabka_kunstmaanbanner_banner_type_media');
           showElem('#hgabka_kunstmaanbanner_banner_type_hoverMedia');
           showElem('#hgabka_kunstmaanbanner_banner_type_imageAlt');
           showElem('#hgabka_kunstmaanbanner_banner_type_imageTitle');
           showElem('#hgabka_kunstmaanbanner_banner_type_url_link_url');
           showElem('#hgabka_kunstmaanbanner_banner_type_newWindow');
-          hideElem('#hgabka_kunstmaanbanner_banner_type_html');
       }
    }).change();
 
